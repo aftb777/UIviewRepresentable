737d475732459cbf1774572a92ae4de694dfa4b7
aftb777 <aftaabmulla228@gmail.com>
2025-09-28 08:27:50 +0530

    Coordinator and Delegates

--- UIviewRepresentable/PractiseTextField .swift
+++ UIviewRepresentable/PractiseTextField .swift
@@ -9,10 +9,28 @@ import SwiftUI
 struct PractiseTextField: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         let TextField = UITextField()
+        TextField.delegate = context.coordinator // Coordinator deleate instance
         return TextField
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+}
+
+// Delegate to allow only numbers in textfield
+
+class Coordinator : NSObject, UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string.isEmpty {
+            return true
+        }
+        
+        return Int(string) != nil
     }
 }
