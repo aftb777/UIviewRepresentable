817085fde46d7ecfa92f495a4e54cce1bd9f55e7
aftb777 <aftaabmulla228@gmail.com>
2025-09-28 08:50:45 +0530

    Bindings and uiUpdateView

--- UIviewRepresentable/ContentView.swift
+++ UIviewRepresentable/ContentView.swift
@@ -6,9 +6,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value: String = ""
+    
     var body: some View {
         VStack {
-            PractiseTextField()
+            
+            Text(value)
+            
+            PractiseTextField(value: $value)
                 .frame(width: 300, height: 50)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
--- UIviewRepresentable/PractiseTextField .swift
+++ UIviewRepresentable/PractiseTextField .swift
@@ -7,30 +7,45 @@ import Foundation
 import SwiftUI
 
 struct PractiseTextField: UIViewRepresentable {
-    func makeUIView(context: Context) -> some UIView {
+    
+    @Binding var value : String
+    
+    func makeUIView(context: Context) -> UITextField {
         let TextField = UITextField()
         TextField.delegate = context.coordinator // Coordinator deleate instance
         return TextField
     }
     
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = value
     }
     
     func makeCoordinator() -> Coordinator {
-        return Coordinator()
+        return Coordinator(value: $value)
     }
 }
 
 // Delegate to allow only numbers in textfield
 
 class Coordinator : NSObject, UITextFieldDelegate {
+    
+    @Binding var value : String
+    
+    init(value: Binding<String>) {
+        self._value = value
+    }
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         if string.isEmpty {
             return true
         }
         
-        return Int(string) != nil
+        if Int(string) != nil {
+            value = textField.text ?? ""
+            return true
+        }
+        
+        return false
     }
 }
